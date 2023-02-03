cdbe512df0f358664fd70ecc47c97f21578325b8
Breno Nascimento <satsukebare23@gmail.com>
2023-02-03 10:33:55 -0300

    add transparent background color in hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Hero from '@/components/Hero/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Dermilton landing page</h1>
+      <Hero/>
     </main>
   )
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from './styles.module.scss';
 
 export default function Hero() {
   return (
-    <section>
-      <h1>Hero!</h1>
-    </section>
+    <main className={styles.hero_container}>
+      <section className={styles.background_color_contrast}/>
+      <section className={styles.hero_center}>
+        <h1>Hero!</h1>
+      </section>
+    </main>
   )
 }
